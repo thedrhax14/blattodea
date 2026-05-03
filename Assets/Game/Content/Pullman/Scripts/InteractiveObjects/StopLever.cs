c7f7df822fbc4dd9bf4c817a0cad5bb349a944fe
thedrhax14 <mishamrulla@gmail.com>
2026-05-03 21:52:25 +0400

    Fix corrupted StopLever merge result

--- Assets/Game/Content/Pullman/Scripts/InteractiveObjects/StopLever.cs
+++ Assets/Game/Content/Pullman/Scripts/InteractiveObjects/StopLever.cs
@@ -5,56 +5,49 @@ public class StopLever : MonoBehaviour, IInteractable
     [SerializeField]
     AudioSource audioSource;
     [SerializeField]
-    using UnityEngine;
+    new SimpleAnimation animation;
+    [SerializeField]
+    InteractObjectData interactObjectData;
 
-    public class StopLever : MonoBehaviour, IInteractable
+    IInteractable.IObjectData IInteractable.ObjectData => interactObjectData;
+    bool IInteractable.CanShow => !GameStates.Instance.StopLeverActivated;
+
+    private void OnEnable()
     {
-        [SerializeField]
-        AudioSource audioSource;
-        [SerializeField]
-        new SimpleAnimation animation;
-        [SerializeField]
-        InteractObjectData interactObjectData;
-        IInteractable.IObjectData IInteractable.ObjectData => interactObjectData;
-        bool IInteractable.CanShow => !GameStates.Instance.StopLeverActivated;
+        GameEvents.Instance.StopLeverActivated += OnStopLeverActivated;
+    }
 
-        private void OnEnable()
-        {
-            GameEvents.Instance.StopLeverActivated += OnStopLeverActivated;
-        }
+    private void OnDisable()
+    {
+        GameEvents.Instance.StopLeverActivated -= OnStopLeverActivated;
+    }
 
-        private void OnDisable()
+    void IInteractable.Interact()
+    {
+        if (!GameStates.Instance.StopLeverActivated)
         {
-            GameEvents.Instance.StopLeverActivated -= OnStopLeverActivated;
-        }
-
-        void IInteractable.Interact()
-        {
-            if (!GameStates.Instance.StopLeverActivated)
+            if (PullmanSequenceNetwork.TryGetInstance(out PullmanSequenceNetwork sequenceNetwork))
             {
-                if (PullmanSequenceNetwork.TryGetInstance(out PullmanSequenceNetwork sequenceNetwork))
-                {
-                    sequenceNetwork.RequestStopLeverActivation();
-                }
-                else
-                {
-                    GameEvents.Instance.ActivateStopLever();
-                }
+                sequenceNetwork.RequestStopLeverActivation();
             }
-        }
-
-        void IInteractable.Stop()
-        {
-        }
-
-        private void OnStopLeverActivated()
-        {
-            audioSource.Play();
-            if (animation != null)
+            else
             {
-                animation.Animation.ChangeDirection(true);
-                animation.Play();
+                GameEvents.Instance.ActivateStopLever();
             }
         }
     }
+
+    void IInteractable.Stop()
+    {
+    }
+
+    private void OnStopLeverActivated()
+    {
+        audioSource.Play();
         if (animation != null)
+        {
+            animation.Animation.ChangeDirection(true);
+            animation.Play();
+        }
+    }
+}
